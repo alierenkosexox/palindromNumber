9d3c99930c4750947a5feea6da2e087a6023ad7b
Ali Eren KÖSE <alierenkose@gmail.com>
2024-02-28 23:32:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+// Ali Eren KÖSE 28/02/2024 23:31
+
+import java.util.Scanner;
+
+public class Main {
+    static boolean isPalindrom(int number) {
+        int temp = number, revesreNumber = 0, lastNumber;
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            revesreNumber = (revesreNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+        if (number == revesreNumber) {
+            System.out.println(number + " Sayısı Palindrom bir sayıdır.");
+            return true;
+        } else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir Saylı Giriniz : ");
+        int number = input.nextInt();
+        System.out.println(isPalindrom(number));
+
+    }
+}
